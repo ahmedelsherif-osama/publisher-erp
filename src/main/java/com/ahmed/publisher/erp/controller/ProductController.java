a057d0fa3faf1a6fdde094ed7fbcc9f4f64f5e43
rem <ahmedelsherif.osama@gmail.com>
2025-11-20 05:09:00 +0200

    fix(Product): update price to use BigDecimal for accuracy

--- src/main/java/com/ahmed/publisher/erp/controller/ProductController.java
+++ src/main/java/com/ahmed/publisher/erp/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.ahmed.publisher.erp.controller;
 
-import com.ahmed.publisher.erp.entity.Product;
-import com.ahmed.publisher.erp.repository.ProductRepository;
+import com.ahmed.publisher.erp.dto.CreateProductRequest;
+import com.ahmed.publisher.erp.dto.PatchProductRequest;
+import com.ahmed.publisher.erp.dto.ProductDto;
+import com.ahmed.publisher.erp.dto.UpdateProductRequest;
+
 import com.ahmed.publisher.erp.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +23,35 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> getAll(){
-        return service.getAllProducts();
+    public ResponseEntity<List<ProductDto>> getAll(){
+        return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping("/{id}")
-    public Product getById(@PathVariable UUID id){
-        return service.getById(id);
+    public ResponseEntity<ProductDto> getById(@PathVariable UUID id){
+
+        return ResponseEntity.ok(service.findById(id));
     }
 
     @PostMapping
-    public Product create(@RequestBody Product product){
-        return service.saveProduct(product);
+    public ResponseEntity<ProductDto> create(@RequestBody CreateProductRequest request){
+        ProductDto created = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+    @PutMapping
+    public ResponseEntity<ProductDto> update(@RequestBody UUID id,@RequestBody UpdateProductRequest request){
+        return ResponseEntity.ok(service.update(id,request));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ProductDto> updatePartially(@RequestBody UUID id,@RequestBody PatchProductRequest request){
+        return ResponseEntity.ok(service.patch(id,request));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        service.delete(id);
+        return  ResponseEntity.noContent().build();
     }
 
 }
