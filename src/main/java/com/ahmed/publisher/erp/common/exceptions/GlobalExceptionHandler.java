6f427a7025a357a450513fb383025cf838fa5819
rem <ahmedelsherif.osama@gmail.com>
2025-11-28 08:40:21 +0200

    feat(handleValidation): add handleValidation function to return field specific error messages for http requests

--- src/main/java/com/ahmed/publisher/erp/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ahmed/publisher/erp/common/exceptions/GlobalExceptionHandler.java
@@ -4,16 +4,38 @@ import org.springframework.boot.context.config.ConfigDataResourceNotFoundExcepti
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomErrorResponse> handleValidation(MethodArgumentNotValidException ex,HttpServletRequest request
+    ) {
+        String messages = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(f -> f.getField() + ": " + f.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        CustomErrorResponse error = new CustomErrorResponse(
+                "Validation failed: " + messages,
+                LocalDateTime.now(),
+                request.getRequestURI()
+
+
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomErrorResponse> handleResourceNotFound(
@@ -28,7 +50,7 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
-    
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneric(Exception ex, HttpServletRequest request){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
